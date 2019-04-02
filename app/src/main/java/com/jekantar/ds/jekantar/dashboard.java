6f93bc282e221b68fccf3a714427302d2ff20132
dickysptra10 <dickyosprint@gmail.com>
2019-04-02 14:33:10 +0700

    Signed-off-by: dickysptra10 <dickyosprint@gmail.com>

---
+++ app/src/main/java/com/jekantar/ds/jekantar/dashboard.java
@@ -0,0 +1,13 @@
+package com.jekantar.ds.jekantar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+    }
+}
